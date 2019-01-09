b9ffe363641c604b7170af3917bc8c65ac9ab76c
MK_Devil <MK_Devil@163.com>
2019-01-09 15:02:21 +0800

    第八章习题

---
+++ 学习/习题/第8章习题.py
@@ -0,0 +1,21 @@
+# 1、包含五个整数0的列表
+# 乘法运算
+list1 = [0] * 5
+print(list1)
+
+# 列表解析
+list2 = [0 for i in range(0, 5)]
+print(list2)
+
+# append
+list3 = []
+for i in range(0, 5):
+    list3.append(0)
+print(list3)
+
+# 2、两个键'a', 'b' 值都为0
+# 字典解析
+dict1 = {x: 0 for x in ('a', 'b')}
+print(dict1)
+
+#
