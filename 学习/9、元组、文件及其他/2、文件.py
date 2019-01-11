dfa0c988ffcc4885126685bd56da2769638ff5bb
MK_Devil <MK_Devil@163.com>
2019-01-11 16:29:40 +0800

    添加文件的内置方法等

--- 学习/9、元组、文件及其他/2、文件.py
+++ 学习/9、元组、文件及其他/2、文件.py
@@ -13,22 +13,74 @@ print('-' * 20, '打开文件', '-' * 20)
 # 加 + 读写模式
 # 加 b 二进制模式
 # python中的文件对象自带迭代器，以行为单位进行迭代，可以使用for line in file
+"""
+with open('test.txt', 'r') as f:
+    array = f.readlines()
+    print(array)
+    res = 0
+    for x in array:
+        a = float(x.rstrip())
+        res += a
+    print(res)
+"""
+# --------------------------------------------------------
+# --------------------    信息查询    --------------------
+# --------------------------------------------------------
+f = open('test.txt', 'r+')
+print(dir(f))
+# fileno() 返回一个整型的文件描述符(file descriptor FD 整型)，可用于底层操作系统的 I/O 操作
+print('文件名为：\t', f.name)
+print('打开方式为：\t', f.mode)
+print('文件描述为：\t', f.fileno())
+
+# isatty() 检测文件是否连接到一个终端设备，是则返回True，否则返回False
+if f.isatty:
+    print('文件%s已连接到一个终端设备！' % f.name)
+else:
+    print('文件%s未连接到一个终端设备！' % f.name)
+
+# closed 文件是否关闭
+if f.closed:
+    print('文件%s已关闭！' % f.name)
+else:
+    print('文件%s未关闭！' % f.name)
+
+# encoding 获取文件编码
+print('%s的编码格式为%s' % (f.name, f.encoding))
+print(f.newlines)
+f.close()
+
+# --------------------------------------------------------
+# --------------------    文件读取    --------------------
+# --------------------------------------------------------
+f = open('test.txt', 'r+')
+
+# readline() 从当前指针开始，读取一行
+print('读取文件的第一行：\n\t', f.readline())
+
+# readlines() 从当前指针开始，读取所有行
+print('读取所有行：\n', f.readlines())
+
+# seek(offset[, whence]) 移动文件读取指针到指定位置
+# offset 偏移量
+# whence 从哪个地方开始计算偏移，默认 0 文件开头，1 当前位置，2 文件末尾
 
 
-with open('test.txt', 'w+') as f:
-    f.write('hello!\n')
-    f.write('goodbye\n')
-    f.flush()
-    print(f.readline(), end='')
-    print(f.readline(), end='')
+# read([size]) 从当前指针开始，读取指定的字节数，如果不指定或提供负值，则读取所有
+print('从当前指针开始，读取9个字节：\n', f.read(9))
 
-# with open('test.txt', 'w') as f:
-#     f.write('hello test file!\n')
-#     f.write('goodbye test file!\n')
+# newlines 当前指针后是否还有行
 
-# with open('test.txt', 'r') as f:
-#     for line in f:
-#         print(line, end = '')  # 不换行，end = '' 取代读取到的 '\n'
-
-# with open('test.txt', 'a') as f:
-#     f.write('ass we can!')
+f.close()
+# --------------------------------------------------------
+# --------------------    文件写入    --------------------
+# --------------------------------------------------------
+# flush() 刷新文件内部缓冲，将缓冲区数据写入文件
+"""
+with open('test.txt', 'w') as f:
+    for i in range(0, 3):
+        content = input('请输入内容：\n')
+        f.write(content)
+        if i == 2:
+            f.flush()
+"""
--- 学习/9、元组、文件及其他/3、struct - 打包.py
+++ 学习/9、元组、文件及其他/3、struct - 打包.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 import struct
+import ctypes
 # struct 可以将任意数据类型与bytes类型转换
 
 
@@ -17,26 +18,55 @@ print("格式化字符串内容为'{}'，格式化的数据大小为{}bytes".for
     format_string, format_size))
 
 
+# ----------------------------------------------------
 # --------------------    打包    --------------------
-print('-' * 20, '    打包    ', '-' * 20)
+# ----------------------------------------------------
+print('-' * 30, '    打包    ', '-' * 30)
+
 # --------------------    方法一、使用struct.pack()方法    --------------------
 # struct.pack(format, v1, v2, ...) 使用format格式进行打包，format与后面的数据数量必须一致
+print('-' * 20, '    方法一    ', '-' * 20)
 format_string = '9sfi'
 print("格式化字符串内容为：\t%s\n格式化字符串长度为：\t%s" %
       (format_string, struct.calcsize(format_string)))
 intro_struct_1 = struct.pack(format_string, '孟令珂'.encode(), 24.5, 3000)
-print('打包完毕！结果为：\t%s\n打包后的数据长度为\t%s' % (intro_struct_1, len(intro_struct_1)))
+print('打包完毕！结果为：\t%s\n打包后的数据长度为：\t%s' % (intro_struct_1, len(intro_struct_1)))
+
 # --------------------    方法二、创建一个带格式化字符串的struct对象    --------------------
 # obj.pack(v1, v2, ...)
+print('-' * 20, '    方法二    ', '-' * 20)
 struct_obj = struct.Struct('9sfi')
 print("格式化字符串内容为：\t%s\n格式化字符串长度为：\t%s" %
       (struct_obj.format, struct_obj.size))
 intro_struct_2 = struct_obj.pack('孟令珂'.encode(), 24.5, 3000)
 print('打包完毕！结果为：\t%s\n打包后的数据长度为\t%s' % (intro_struct_2, len(intro_struct_2)))
 
+# --------------------    方法三、使用struct.pack_into()方法    --------------------
+# struct.pack_into(format, buffer, offset, v1, v2, ...)
+# offset 偏移，一般情况下0即可
+# buffer为可变字符缓冲区，可以避免内存的浪费
+# 创建可变字符缓冲区，要与pack后的大小一致
+print('-' * 20, '    方法三    ', '-' * 20)
+buff_3 = ctypes.create_string_buffer(struct.calcsize(format_string))
+values = ('孟令珂'.encode(), 24.5, 3000)
+intro_struct_3 = struct.pack_into(format_string, buff_3, 0, *values)
+print('打包完毕！结果为：\t%s' % intro_struct_3)
+print('数据已经存入可变字符串缓冲区中！')
 
+# --------------------    方法四、使用obj.pack_into()方法    --------------------
+# obj.pack_into(buffer, offset, v1, v2, ...)
+print('-' * 20, '    方法四    ', '-' * 20)
+buff_4 = ctypes.create_string_buffer(struct_obj.size)
+intro_struct_4 = struct_obj.pack_into(buff_4, 0, *values)
+print('打包完毕！结果为：\t%s' % intro_struct_4)
+print('数据已经存入可变字符串缓冲区中！')
+
+
+# ----------------------------------------------------
 # --------------------    解包    --------------------
-print('-' * 20, '    解包    ', '-' * 20)
+# ----------------------------------------------------
+print('-' * 30, '    解包    ', '-' * 30)
+
 # --------------------    方法一、使用struct.unpack()方法    --------------------
 # unpack(format, buffer)
 intro_struct_1_unpack = list(struct.unpack(format_string, intro_struct_1))
@@ -48,3 +78,15 @@ print("解包完毕，结果为：\t{}".format(tuple(intro_struct_1_unpack)))
 intro_struct_2_unpack = list(struct_obj.unpack(intro_struct_2))
 intro_struct_2_unpack[0] = intro_struct_2_unpack[0].decode()
 print("解包完毕，结果为：\t{}".format(tuple(intro_struct_2_unpack)))
+
+# --------------------    方法三、使用struct.unpack_from()方法    --------------------
+# struct.unpack_from(format, buffer, offset) 偏移默认为0
+intro_struct_3_unpack = list(struct.unpack_from(format_string, buff_3))
+intro_struct_3_unpack[0] = intro_struct_3_unpack[0].decode()
+print("解包完毕，结果为：\t{}".format(tuple(intro_struct_3_unpack)))
+
+# --------------------    方法四、使用obj.unpack_from()方法    --------------------
+# obj.unpack_from(buffer, offset) 偏移默认为0
+intro_struct_4_unpack = list(struct_obj.unpack_from(buff_4))
+intro_struct_4_unpack[0] = intro_struct_4_unpack[0].decode()
+print("解包完毕，结果为：\t{}".format(tuple(intro_struct_4_unpack)))
