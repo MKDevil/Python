3fa1bbaed5c7eb83485adbf16920211e64ebc2a7
MK_Devil <MK_Devil@163.com>
2019-01-17 15:23:32 +0800

    十二章习题

--- 学习/12、if测试和语法规则/1、if语句.py
+++ 学习/12、if测试和语法规则/1、if语句.py
@@ -1,3 +1,91 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+# ------------------------------------------------------------------
+# --------------------    实现switch类似功能    --------------------
+# ------------------------------------------------------------------
+# 实例1
+choice = 'ham'
+print({'spam': 1, 'ham': 1.99, 'eggs': 3, 'bacon': 1.1}[choice])
+# 等价于
+if choice == 'spam':
+    print(1)
+elif choice == 'ham':
+    print(1.99)
+elif choice == 'eggs':
+    print(3)
+elif choice == 'bacom':
+    print(1.1)
+else:
+    print('Error')
+
+# 实例2，加减乘除的实现1
+
+
+def add(num1, num2):
+    return num1 + num2
+
+
+def sub(num1, num2):
+    return num1 - num2
+
+
+def mul(num1, num2):
+    return num1 * num2
+
+
+def div(num1, num2):
+    return num1 / num2
+
+
+operator = {'+': add, '-': sub, '*': mul, '/': div}
+res = operator['+'](1, 2)
+print(res)
+
+# 实例3，加减乘除的实现2
+
+
+def adde():
+    return a + b
+
+
+def sube():
+    return a - b
+
+
+def mule():
+    return a * b
+
+
+def dive():
+    return a / b
+
+
+a = 5
+b = 7
+operator = {'+': adde(), '-': sube(), '*': mule(), '/': dive()}
+res = operator['*']
+print(res)
+
+# 更多用法，请看"V - 函数\\switch语句的实现.py"
+print("更多用法，请看'V - 函数\\switch语句的实现.py'")
+
+# ------------------------------------------------------------------
+# -----------------------    if的通用格式    -----------------------
+# ------------------------------------------------------------------
+# if 的通用格式
+# if <test1>:
+#     <statement1>
+# elif <test2>:
+#     <statement2>
+# else:
+#     <statement3>
+
+
+# ------------------------------------------------------------------
+# ----------------------    if的三元表达式    ----------------------
+# ------------------------------------------------------------------
+# A = X if Y else Z
+b = 'spams'
+a = 'temp' if b == 'spam' else 'test'
+print(a)
---
+++ 学习/习题/第12章习题.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# 1、python中怎样编写多路分支？
+# if/elif/else
+
+# 2、怎样将 if/else 语句写成表达式？
+# 三元表达式，X if Y else Z
+
+# 3、怎样使单个语句跨过多行？
+# 第一：\
+# 第二：各种括号()[]{}
+
+# 4、True 和 False 的含义
+# 布尔真和布尔假
