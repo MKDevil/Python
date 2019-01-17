0dc0627f131f000fd942a36cf7c065e8f707ac5f
MK_Devil <MK_Devil@163.com>
2019-01-17 16:53:53 +0800

    for循环的几个示例

--- 学习/13、while和for循环/1、while循环.py
+++ 学习/13、while和for循环/1、while循环.py
@@ -1,25 +1,16 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-# 实例1，切掉字符串
+# 示例1，切掉字符串
 s = 'spam'
 while s:
     print(s)
     s = s[1:]
 
-# 实例2，循环计算
+# 示例2，循环计算
 a = 0
 b = 10
 while a < b:
     print(a, end=' ')
     a += 1
 print('\n')
-
-# 实例3，跳出循环
-res = a = 0
-while True:
-    a += 1
-    res += a
-    if a == 50:
-        break
-print(res)
--- 学习/13、while和for循环/2、break、continue、pass和循环else.py
+++ 学习/13、while和for循环/2、break、continue、pass和循环else.py
@@ -1,2 +1,45 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+
+# break             跳出最近所在的循环
+# continue          跳到最近所在循环的开头处（来到循环的首行）
+# pass              占位语句，什么事也不做
+# 循环else模块      只有当循环正常离开时才会执行（没有触发break）
+
+# 示例，break
+res = i = 0
+while True:
+    i += 1
+    res += i
+    if i == 100:
+        break
+print(res)
+
+# 示例，continue
+# 求1~100之内的奇数的和
+res = i = 0
+while i < 100:
+    i += 1
+    if i % 2 == 0:
+        continue
+    res += i
+print(res)
+
+# 示例，循环else模块
+res = i = 0
+while i < 100:
+    i += 1
+    res += i
+else:
+    print(res)
+
+# 示例，判断质数
+for y in range(2, 100):
+    x = y // 2
+    while x > 1:
+        if y % x == 0:
+            # print(y, 'has a factor:', x)
+            break
+        x -= 1
+    else:
+        print(y, '是一个质数')
