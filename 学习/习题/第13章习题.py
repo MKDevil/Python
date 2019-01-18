b8f194c9e94c57496035e35436ac70913b305b83
MK_Devil <MK_Devil@163.com>
2019-01-18 15:25:00 +0800

    13章习题

---
+++ 学习/习题/第13章习题.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# 1、while和for循环的区别？
+# 都可以用于循环，但while是更加通用的循环模式，for更常用与迭代和遍历
+
+# 2、break和continue之间的区别？
+# break触发后，直接跳出当前的循环
+# continue触发后，忽略后续语句，从当前循环的首行继续执行，迭代对象会迭代到下一个
+
+# 3、一个循环语句的else分句何时执行？
+# 当不满足循环条件时执行，即循环结束时执行
+# 可以被break语句跳过
+
+# 4、怎样编写一个基于计数器的循环？
+# 两种方法
+# i = 0
+# while True:
+#     i += 1
+# for i in range():
+#     pass
+
+# 5、怎样使range用于for循环中
+# for x in range():
+#     pass
