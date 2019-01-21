f53f951a8436f0a26c1ee318894d21c88f449cfc
MK_Devil <MK_Devil@163.com>
2019-01-21 15:49:49 +0800

    字典，集合等多种解析示例

--- 学习/14、迭代器和解析（一）/4、字典和集合的解析.py
+++ 学习/14、迭代器和解析（一）/4、字典和集合的解析.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+li1 = ['a', 'b', 'c']
+li2 = [65, 66, 67]
+
+# 列表解析
+li = [x + str(y) for x, y in zip(li1, li2)]
+print(li)
+
+# 元组解析
+t = tuple(i for i in list(map(lambda x, y: str(ord(x)) + str(y), li1, li2)))
+print(t)
+
+# 集合解析
+s = {ord(x) + y for x, y in zip(li1, li2)}
+print(s)
+
+# 字典解析
+d = {x: chr(y) for x, y in zip(li1, li2)}
+print(d)
