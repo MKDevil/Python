d40cd0790bf6ac0dd29866c0a9cbbbccf3551c96
MK_Devil <MK_Devil@163.com>
2019-01-23 11:41:44 +0800

    作用域 - 闭包的几个例子

--- 学习/4、第四部分 - 函数/17、作用域/1、作用域.py
+++ 学习/4、第四部分 - 函数/17、作用域/1、作用域.py
@@ -55,20 +55,64 @@ def fun1():
 
     def fun2():
         print(x)
-    fun2()
+    fun2()  # 在 fun2() 运行时，向上查找变量 x 最终查找到 fun1() 的 x = 102
 
 
 fun1()
 
 
 # 示例，嵌套作用域------------------------------------------------------
+"""
+在这个例子中，x = 123 是函数 f2() 的环境变量，与其在 f2() 的先后顺序无关
+action = f1() 即运行 f1() 函数，将 f2() 函数返回给 action，将 action 变为 f2() 函数
+同时，给 action() 函数设定一个 x = 123 的环境变量
+"""
+
+
 def f1():
-    x = 123
+    # x = 123
 
     def f2():
-        print('faq', x)
+        print('faq', x + 10)
+    x = 123
     return f2
 
 
 action = f1()
 action()
+
+
+# 示例，闭合函数（工厂函数）
+"""
+m = maker(2) 执行时，将函数名 action 返回给 m，并且将变量 x 的值设置为 2 作为 action 的环境变量
+m(3) 执行时，自动赋值 y = 3，同时查找变量 x，向上一级找到执行 m = maker(2) 时记录的 x = 2
+"""
+
+
+def maker(x):
+    def action(y):
+        return x ** y
+    return action
+
+
+m = maker(2)
+print(m(3))
+
+
+# 示例，闭合函数，使用lambda
+def maker(x):
+    action = (lambda y: x ** y)  # 在低版本 python 中，会报错
+    return action
+
+
+f = maker(3)
+print(f(2))
+
+
+def maker(x):
+    action = (lambda y, x=x: x ** y)  # 这种方法可以在任何版本的 python 中使用
+    return action
+
+
+f = maker(3)
+print(f(3))
