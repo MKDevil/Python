51b2c6223ed27d6929ab367edbbd992513549502
MK_Devil <MK_Devil@163.com>
2019-01-24 08:58:10 +0800

    多了一个主意事项

--- 学习/4、第四部分 - 函数/17、作用域/3、nonlocal语句.py
+++ 学习/4、第四部分 - 函数/17、作用域/3、nonlocal语句.py
@@ -4,7 +4,7 @@
 """
 nonlocal 类似于 global，声明了将要在一个嵌套的函数作用域中的一个名称
 nonlocal 仅限于向上一层的 def 作用域
-nonlocal 只能存在于嵌套的 def 中
+nonlocal 只能存在于嵌套的 def 中，且上层 def 必须定义了指定变量，否则报错
 """
 
 
@@ -25,6 +25,8 @@ except Exception as e:
     print('出现错误：', e)
 
 # 示例二，修改
+
+
 def tester(start):
     state = start
 
@@ -36,6 +38,24 @@ def tester(start):
 
 
 f = tester(0)
+f('spam')
+f('ham')
+f('faq')
+
+# 示例三，不在上层 def 定义 nonlocal 指定的变量
+state = 0
+
+
+def tester():
+
+    def nested(label):
+        nonlocal state
+        print(label, state)
+        state += 1
+    return nested
+
+
+f = tester()
 f('spam')
 f('ham')
 f('faq')
