f58b0ece3b9abcaad702d2d16a569e7d2bfdba4a
MK_Devil <MK_Devil@163.com>
2019-02-14 15:21:58 +0800

    计时器，暂时没看懂

--- 学习/4、第四部分 - 函数/20、迭代和解析（二）/mytimer.py
+++ 学习/4、第四部分 - 函数/20、迭代和解析（二）/mytimer.py
@@ -1,14 +1,42 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 import time
+
 reps = 1000
-resplist = range(reps)
+repslist = range(reps)
 
 
+# 计时器 v1.0 ------------------------------------------------------------------
 def timer(func, *pargs, **kargs):
     """使用该函数运行指定函数1000次并计时"""
     start = time.process_time()
-    for i in resplist:
+    for i in repslist:
         ret = func(*pargs, **kargs)
     elapsed = time.process_time() - start
     return (elapsed, ret)
+
+
+# 计时器 v2.0 ------------------------------------------------------------------
+def trace(*args):
+    pass
+
+
+def adv_timer(func, *pargs, **kargs):
+    _reps = kargs.pop('_reps', 1000)
+    trace(func, pargs, kargs, _reps)
+    repslist = range(_reps)
+    start = time.process_time()
+    for i in repslist:
+        ret = func(*pargs, **kargs)
+    elapsed = time.process_time() - start
+    return (elapsed, ret)
+
+
+def best_timer(func, *pargs, **kargs):
+    _reps = kargs.pop('_reps', 50)
+    best = 2 ** 32
+    for i in range(_reps):
+        (time, ret) = adv_timer(func, *pargs, **kargs)
+        if time < best:
+            best = time
+    return (best, ret)
