7e85bae47b9ea49bddf6559c848c840caf435d59
MK_Devil <MK_Devil@163.com>
2019-02-18 14:58:49 +0800

    while...else...循环

--- 学习/3、第三部分 - 语句和语法/13、while和for循环/1、while循环.py
+++ 学习/3、第三部分 - 语句和语法/13、while和for循环/1、while循环.py
@@ -13,3 +13,15 @@ b = 10
 while a < b:
     print(a, end=' ')
     a += 1
+
+# 示例3，while...else...循环
+y = str(input('输入你想测试的数字'))
+x = y // 2
+while x > 1:
+    if y % x == 0:
+        # print(y, 'has a factor:', x)
+        break
+    x -= 1
+else:
+    """当while循环正常结束时（非return、break等语句特殊结束），触发else语句"""
+    print(y, '是一个质数')
