f9d36b2525da223e4730def5e41361e481506452
MK_Devil <MK_Devil@163.com>
2019-03-05 10:56:12 +0800

    第五部分模块，结束

---
+++ 学习/习题/5、第五部分练习题/main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+import mymod
+
+FILE = r'E:\孟令珂\Python\学习\习题\5、第五部分练习题\mymod.py'
+print(mymod.countLines(FILE))
+print(mymod.countChars(FILE))
---
+++ 学习/习题/5、第五部分练习题/mymod.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+该文件用于计算文件中的行数和字符数
+"""
+
+
+def countLines(name: 'file'):
+    count = 0
+    with open(name, 'r', encoding='utf-8') as file:
+        for line in file:
+            count += 1
+    return count
+
+
+def countChars(name: 'file'):
+    count = 0
+    with open(name, 'r', encoding='utf-8') as file:
+        for line in file:
+            count += len(line)
+    return count
+
+
+def test(name):
+    return name, countLines(name), countChars(name)
+
+
+if __name__ == '__main__':
+    file = r'E:\孟令珂\Python\学习\习题\5、第五部分练习题\mymod.py'
+    print(countLines(file))
+    print(countChars(file))
+    print(test(file))
